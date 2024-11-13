2eb25268236c89ca7c4e3f66c2303040a6d76a6c
Manikandan0307 <mrperfectboi789987@gmail.com>
2024-11-13 14:42:52 +0530

    Add files via upload

---
+++ Hotel RoomBooking/src/HotelApp.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class HotelApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Signup signup = new Signup();
+        
+        signup.loadCustomerDetails();
+        
+        Login login = new Login(Signup.customer);
+
+        while (true) {
+            System.out.println("Welcome to the Easy Rest Hotel!");
+            System.out.println("===============================");
+            System.out.println("1) Sign Up");
+            System.out.println("2) Login");
+            System.out.println("3) Exit");
+            System.out.print("Choose an option: ");
+            int option = sc.nextInt();
+            sc.nextLine(); 
+
+            if (option == 1) {
+                signup.signup(sc);  
+            } else if (option == 2) {
+                login.login(sc);  
+            } else {
+                System.out.println("Exiting...");
+                break;
+            }
+        }
+        sc.close();
+    }
+}
